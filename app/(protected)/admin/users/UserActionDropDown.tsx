afb914f8156a6bfd5f773c60f903499b874339a3
VzNon <wardarkly@gmail.com>
2026-01-25 18:38:08 +0700

    แยก component userdropdown

---
+++ app/(protected)/admin/users/UserActionDropDown.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { User } from "@/generated/prisma/client";
+import { useState } from "react";
+import UserManageDialog from "./UserManageDialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { UserPen } from "lucide-react";
+
+export default function UserActionDropDown({ user }: { user: User }) {
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  return (
+    <>
+      <UserManageDialog
+        user={user}
+        open={showEditDialog}
+        onOpenChange={setShowEditDialog}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex justify-center">
+            <Button size="icon-sm">
+              <span className="sr-only">Open menu</span>
+              <UserPen />
+            </Button>
+          </div>
+          {/* <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+              </Button> */}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          {/* <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(user.id)}
+              >
+              Copy payment ID
+              </DropdownMenuItem> */}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => setShowEditDialog(true)}>
+            แก้ไขข้อมูล
+          </DropdownMenuItem>
+          <DropdownMenuItem>จัดการสิทธิ์</DropdownMenuItem>
+          <DropdownMenuItem variant="destructive">ลบ</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
--- app/(protected)/admin/users/columns.tsx
+++ app/(protected)/admin/users/columns.tsx
@@ -25,6 +25,7 @@ import { BadgeCustomColor } from "@/components/ui/badgeCustom";
 import { Checkbox } from "@/components/ui/checkbox";
 import { DataTableColumnHeader } from "@/components/data-table-column-header";
 import { useState } from "react";
+import UserActionDropDown from "./UserActionDropDown";
 
 export const columns: ColumnDef<User>[] = [
   {
@@ -138,44 +139,7 @@ export const columns: ColumnDef<User>[] = [
     header: () => <div className="text-center">Actions</div>,
     cell: ({ row }) => {
       const user = row.original;
-      const [showEditDialog, setShowEditDialog] = useState(false);
-      return (
-        <>
-          <UserManageDialog
-            user={user}
-            open={showEditDialog}
-            onOpenChange={setShowEditDialog}
-          />
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div className="flex justify-center">
-                <Button size="icon-sm">
-                  <span className="sr-only">Open menu</span>
-                  <UserPen />
-                </Button>
-              </div>
-              {/* <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-              </Button> */}
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              {/* <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(user.id)}
-              >
-              Copy payment ID
-              </DropdownMenuItem> */}
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => setShowEditDialog(true)}>
-                แก้ไขข้อมูล
-              </DropdownMenuItem>
-              <DropdownMenuItem>ระงับการใช้งาน(Ban)</DropdownMenuItem>
-              <DropdownMenuItem variant="destructive">ลบ</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </>
-      );
+      return <UserActionDropDown user={user} />;
     },
   },
 ];
